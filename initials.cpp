84c6fca68f5cf41504d627c184bdb735106d7b05
Magda <makneta@gmail.com>
2018-12-13 12:13:32 +0100

    My initials in C++

---
+++ initials.cpp
@@ -0,0 +1,11 @@
+// My initials MR
+#include <iostream>
+int main() {
+  std::cout << "M     M    RRRR\n";
+  std::cout << "MM   MM    R   RR\n";
+  std::cout << "M M M M    R   RR\n";
+  std::cout << "M  M  M    R RR\n";
+  std::cout << "M     M    R   R\n";
+  std::cout << "M     M    R    R\n";
+  std::cout << "M     M    R     R\n"; 
+}
